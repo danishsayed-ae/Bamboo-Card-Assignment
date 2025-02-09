f7c83667156d3df3d4fa46586bd0a564119544d2
Danish Sayed <danishsayed.ae@gmail.com>
2025-02-10 01:04:35 +0400

    Update ShoppingCartPage.js

--- cypress/e2e/page-objects/ShoppingCartPage.js
+++ cypress/e2e/page-objects/ShoppingCartPage.js
@@ -1,5 +1,6 @@
 class ShoppingCartPage {
     clickProceedToCheckout() {
+        cy.wait(3000);
         cy.get('[data-role="proceed-to-checkout"]', { timeout: 6000 }).scrollIntoView().should('be.visible').click();
     }
 }
